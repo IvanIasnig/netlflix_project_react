6611b8edda95e53c44757e46f001645a58ff8fac
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-06-10 05:09:25 +0200

    ciao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import Jumbotron from './components/jumbotron'
+import jumboData from './fixtures/jumbo.json';
 
 export default function App() {
-  return (<div>Ciao</div>);
+  return (
+<Jumbotron.Container>
+  {jumboData.map((item) => (
+    <Jumbotron key={item.id} direction={item.direction}>
+      <p>{item.title}</p>
+      <p>{item.subTitle}</p>
+      <p>{item.image}</p>
+      <p>{item.alt}</p>
+    </Jumbotron>
+  ))}
+</Jumbotron.Container>
+  );
 }
